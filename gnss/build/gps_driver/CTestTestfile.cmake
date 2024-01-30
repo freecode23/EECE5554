9a7090defe7fb5df22e8f4b033f4aa72287e9d6f
Sherly Hartono <sharton1@asu.edu>
2024-01-29 20:45:36 -0500

    Fix UTC conversion to epoch

--- gnss/build/gps_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sherly/Desktop/code/EECE5554/gnss/src/gps_driver
-# Build directory: /home/sherly/Desktop/code/EECE5554/gnss/build/gps_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
